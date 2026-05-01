11ebdb45990f7521e4404f455af16eb37e94ddf1
马子昂 <929110464@qq.com>
2026-05-01 21:59:05 +0800

    Initial Commit

---
+++ ZenKith/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZenKith
+//
+//  Created by 马子昂 on 2026/5/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZenKith/ZenKithApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenKithApp.swift
+//  ZenKith
+//
+//  Created by 马子昂 on 2026/5/1.
+//
+
+import SwiftUI
+
+@main
+struct ZenKithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
